185eb81ae862da22209b74db94650fda3c052e0d
Waseem Mohammed <waseemm@chimeratechnologies.com>
2024-03-07 14:29:02 +0530

    testing yml file

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       <p>Enter the username</p>
        <input type="text" name='username' placeholder='username' />
+       <p>Lets see new things here from now on</p>
     </div>
   )
 }
