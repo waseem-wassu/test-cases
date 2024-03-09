1ff4f1f5bf9db06211b37114c82e988b276e31cd
Waseem Mohammed <waseemm@chimeratechnologies.com>
2024-03-10 00:43:52 +0530

    to get single and multiple elements

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,39 +1,108 @@
-import { fireEvent, render,screen } from "@testing-library/react"
+//Chapter-4 is all about theoritical part where get more insights of the pattern that we are following during this
+
 import App from "./App"
+import {render, screen} from "@testing-library/react"
+
+/* 
+                       Steps in testing UI
+
+1.Render Component 
+2.Find element and action
+3.Assertions
+*/
+
 /*
-test("checking the form",() => {
-    //first jis component ku test karthin use render field mein likhlena
-    render(<App/>)
-    //ab usey find karna screen mein jo hamna hona
-    let title1 = screen.getByText('Enter the username')
-    //ab woh hai ya nai dekhna App component mein
-    expect(title1).toBeInTheDocument()
+Differnts methods to find RTL elements 
+
+1.By element type
+2.By element name
+3.By element id
+4.By test id
+etc
+*/
+
+/*
+
+Types of RTL queries
+
+1.Find Single Element
+ -a:getBY
+ -b:queryBy
+ -c:findBy
+
+2.Find multiple Element
+ -a:getAllBy
+ -b:QueryAllBy
+ -c:findAllBy
+ */
+
+//This will be very helpful for your interviews
+
+// let starts with single elements
+
+//*************************getByRole Query***********************************
+test("Checking if button is available", () => {
+  render(<App />);
+
+  /*************************************** if you have only one button in your testing component****************** */
+    // let findOutButton = screen.getByRole("button");
+    // expect(findOutButton).toBeInTheDocument();
+    //if I have only one button at that time I can use this role directly 
+    
+  /*************************************** if you have more than one button in your testing component****************** */
+  let findOutButton = screen.getByRole("button",{name:"Click"});
+  let findOutButton2 = screen.getByRole("button",{name:"Button 2"});
+
+  expect(findOutButton).toBeInTheDocument();
+  expect(findOutButton2).toBeInTheDocument();
+});
+
+
+test("Testing custom roles", () => {
+  render(<App />);
+  let checkCustomRole = screen.getByRole("test-role");
+  expect(checkCustomRole).toBeInTheDocument();
+});
+
+
+
+
+
+//now lets test multiple line element
+
+//*************************getAllByRole Query***********************************
+test("Checking if button is available", () => {
+  render(<App />);
+  let findAllButtonAtOnce = screen.getAllByRole("button");
+
+  // Here we get the output as an array
+
+  //we can achieve this with various merthod
+
+  //Method-1 : by accessing single elements with the help of index of an array
+
+  // console.log(findAllButtonAtOnce, "checks whats here");
+  expect(findAllButtonAtOnce[0]).toBeInTheDocument();
+
+  //Method-2 : by looping an array to get the access to the elements dynamically
+
+  for(let i=0; i< findAllButtonAtOnce.length; i++) {
+    expect(findAllButtonAtOnce[i]).toBeInTheDocument();
+  }
+});
+
+test("Lets try to test Select component",() => {
+     render(<App/>)
+     let optionsArray= screen.getAllByRole('option')
+     for(let i=0; i< optionsArray.length; i++) {
+      expect(optionsArray[i]).toBeInTheDocument();
+    }
 })
 
-test("checking input field",() => {
-  //first jis component ku test karthin use render field mein likhlena
-  render(<App/>)
-  //ab usey find karna screen mein jo hamna hona
-   let findInputField = screen.getByRole("textbox")
-   //isey kaisa b define kar sakthin ya phir upar ka ya nicche ka
-   let findInputField2 = screen.getByPlaceholderText("username")
-  //ab woh hai ya nai dekhna App component mein
-  expect(findInputField).toBeInTheDocument()
-}) */
+//Note:  
+// Still we have many more things here but I will try to cover that in a next chapter may be with the actual project
 
-//ab yahan se input field ku change karne ka 
+//till now we have covered only synchronous cases 
 
 
-test("checking input field",() => {
-  //first jis component ku test karthin use render field mein likhlena
-  render(<App/>)
-  //ab usey find karna screen mein jo hamna hona
-   let findInputField = screen.getByRole("textbox")
-   //ye onchange event ku
-   fireEvent.change(findInputField,{target:{value:"a"}})
-  //ab woh hai ya nai dekhna App component mein
-  expect(findInputField).toHaveValue("a")
-})
-
-//ab button ka submit check karinge
 
--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <p>Enter the username</p>
-       <input type="text" name='username' placeholder='username' />
-       <p>Lets see new things here from now on</p>
+      <h2>Testing on single and multiple elements</h2>
+      <button>Click</button>
+      <button>Button 2</button>
+      {/* we can give custom role in order to access this in spscific element  */}
+      <div role="test-role">
+         hii
+      </div>
+      <div className="select-style">
+          <select name="" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+      </div>
     </div>
   )
 }
