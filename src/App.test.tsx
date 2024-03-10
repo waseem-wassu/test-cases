fcb141985de249028f85266049b362f27a95500c
Waseem Mohammed <waseemm@chimeratechnologies.com>
2024-03-10 16:18:18 +0530

    implementing asynchronus test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,108 +1,32 @@
-//Chapter-4 is all about theoritical part where get more insights of the pattern that we are following during this
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-import App from "./App"
-import {render, screen} from "@testing-library/react"
-
-/* 
-                       Steps in testing UI
-
-1.Render Component 
-2.Find element and action
-3.Assertions
-*/
-
-/*
-Differnts methods to find RTL elements 
-
-1.By element type
-2.By element name
-3.By element id
-4.By test id
-etc
-*/
-
-/*
-
-Types of RTL queries
-
-1.Find Single Element
- -a:getBY
- -b:queryBy
- -c:findBy
-
-2.Find multiple Element
- -a:getAllBy
- -b:QueryAllBy
- -c:findAllBy
- */
-
-//This will be very helpful for your interviews
-
-// let starts with single elements
-
-//*************************getByRole Query***********************************
-test("Checking if button is available", () => {
+test("Initial when page loads", () => {
   render(<App />);
-
-  /*************************************** if you have only one button in your testing component****************** */
-    // let findOutButton = screen.getByRole("button");
-    // expect(findOutButton).toBeInTheDocument();
-    //if I have only one button at that time I can use this role directly 
-    
-  /*************************************** if you have more than one button in your testing component****************** */
-  let findOutButton = screen.getByRole("button",{name:"Click"});
-  let findOutButton2 = screen.getByRole("button",{name:"Button 2"});
-
-  expect(findOutButton).toBeInTheDocument();
-  expect(findOutButton2).toBeInTheDocument();
+  let mainHeading = screen.getByText("findBy and findAllBy");
+  expect(mainHeading).toBeInTheDocument();
 });
 
-
-test("Testing custom roles", () => {
+test("Check the data if API is fetch successfully", async () => {
   render(<App />);
-  let checkCustomRole = screen.getByRole("test-role");
-  expect(checkCustomRole).toBeInTheDocument();
+  let mainHeading = screen.getByText("findBy and findAllBy");
+  expect(mainHeading).toBeInTheDocument();
+
+  //since API is involved here i need to use async now
+  let getTheElement = screen.getByText("bye");
+  expect(getTheElement).toBeInTheDocument();
+
+  //after API loads
+  let getTheElement1 = await screen.findByText("hello");
+  expect(getTheElement1).toBeInTheDocument();
 });
 
+//lets use userEvent here
 
-
-
-
-//now lets test multiple line element
-
-//*************************getAllByRole Query***********************************
-test("Checking if button is available", () => {
+test("User event setup", async() => {
   render(<App />);
-  let findAllButtonAtOnce = screen.getAllByRole("button");
-
-  // Here we get the output as an array
-
-  //we can achieve this with various merthod
-
-  //Method-1 : by accessing single elements with the help of index of an array
-
-  // console.log(findAllButtonAtOnce, "checks whats here");
-  expect(findAllButtonAtOnce[0]).toBeInTheDocument();
-
-  //Method-2 : by looping an array to get the access to the elements dynamically
-
-  for(let i=0; i< findAllButtonAtOnce.length; i++) {
-    expect(findAllButtonAtOnce[i]).toBeInTheDocument();
-  }
+  let btn = screen.getByText("Click Me");
+  await  userEvent.click(btn);
+  expect(screen.getByText("Waseem")).toBeInTheDocument();
 });
-
-test("Lets try to test Select component",() => {
-     render(<App/>)
-     let optionsArray= screen.getAllByRole('option')
-     for(let i=0; i< optionsArray.length; i++) {
-      expect(optionsArray[i]).toBeInTheDocument();
-    }
-})
-
-//Note:  
-// Still we have many more things here but I will try to cover that in a next chapter may be with the actual project
-
-//till now we have covered only synchronous cases 
-
-
-
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const App = () => {
+  const [data,setData] = React.useState<boolean>(false);
+  const [name,setName] = React.useState<string>("")
+  useEffect(() => {
+    setTimeout(() => {
+     setData(true)
+    },500)
+  }, []);
+
+
   return (
     <div>
-      <h2>Testing on single and multiple elements</h2>
-      <button>Click</button>
-      <button>Button 2</button>
-      {/* we can give custom role in order to access this in spscific element  */}
-      <div role="test-role">
-         hii
-      </div>
-      <div className="select-style">
-          <select name="" id="">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-          </select>
-      </div>
+      <h3>findBy and findAllBy</h3>
+      {data ? <h4>hello</h4> : <h4>bye</h4>}
+      {/* this is for user event test case  */}
+      <p>{name}</p>
+      <button onClick={() => setName("Waseem")}>Click Me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
